ba33d089b77494f2d345c1972094a5aa56090c2d
axelet <axelet3@gmail.com>
2017-04-24 16:39:31 +0000

    Done.

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@ cd /home/box/web
 rm /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
-rm /etc/gunicorn.d/hello.py
-ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-/etc/init.d/gunicorn restart
+#rm /etc/gunicorn.d/hello.py
+#ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#/etc/init.d/gunicorn restart
 django-admin startproject ask
 cd /home/box/web/ask
 python manage.py startapp qa
